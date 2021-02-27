7ba2ab2376e03294a8b5b7f489854127d91ee0f0
Arnas Savickas <arnas.savi@gmail.com>
2021-02-27 16:58:43 +0200

    update CollectionsList.test

--- src/components/Dashboard/CollectionsList/CollectionsList.test.tsx
+++ src/components/Dashboard/CollectionsList/CollectionsList.test.tsx
@@ -1,19 +1,18 @@
 import React from 'react';
 
-import {
-  getByLabelText,
-  getByText,
-  render,
-  waitFor,
-  within,
-} from '@testing-library/react';
-import { CollectionDetailsProps } from '../../../types';
+import { render } from '@testing-library/react';
 import user from '@testing-library/user-event';
 import CollectionList from './CollectionsList';
 import { collectionList } from '../../../utils/testUtils';
 import { getCollections, deleteCollection } from '../../../firebase';
 
 jest.mock('../../../firebase');
+const mockHistoryPush = jest.fn();
+jest.mock('react-router-dom', () => ({
+  useHistory: () => ({
+    push: mockHistoryPush,
+  }),
+}));
 
 describe('<CollectionList/>', () => {
   beforeEach(() => {
@@ -22,10 +21,33 @@ describe('<CollectionList/>', () => {
   });
 
   test('renders two rows of collections', async () => {
-    const { getByText, debug, findAllByText } = render(<CollectionList />);
+    const { findAllByText } = render(<CollectionList />);
 
     const titles = await findAllByText(/collection title/);
 
     expect(titles).toHaveLength(2);
+  });
+  test('clicking a row calls history.push with correct collectionId', async () => {
+    const { findByText } = render(<CollectionList />);
+
+    const title = await findByText(/collection title 1/);
+    user.click(title);
+
+    expect(mockHistoryPush).toBeCalledWith('edit/collectionId1');
+  });
+  test('clicking delete button calls deleteCollection with correct collectionId', async () => {
+    const { findAllByRole, getByText } = render(<CollectionList />);
+
+    const deleteBtn = await findAllByRole('button', {
+      name: /delete/i,
+    });
+    user.click(deleteBtn[0]);
+    const yesBtn = getByText('Yes');
+    user.click(yesBtn);
+
+    expect(deleteCollection).toHaveBeenCalledWith(
+      collectionList[0].id,
+      expect.anything()
+    );
   });
 });
