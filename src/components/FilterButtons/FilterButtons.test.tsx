50978525fcc8f6bac83060c2e8130ded38e4f003
Arnas Savickas <arnas.savi@gmail.com>
2021-03-31 21:09:43 +0300

    add tests

---
+++ src/components/FilterButtons/FilterButtons.test.tsx
@@ -0,0 +1,79 @@
+import React from 'react';
+
+import { render } from '@testing-library/react';
+import { FilterButtonsProps } from '../../types';
+import user from '@testing-library/user-event';
+import FilterButtons from './FilterButtons';
+import { collection } from '../../utils/testUtils';
+
+const props: FilterButtonsProps = {
+  collection,
+  setFilteredPhotos: jest.fn(),
+};
+
+const propsModifyLightbox: FilterButtonsProps = {
+  collection,
+  setFilteredPhotos: jest.fn(),
+  modifyLightbox: true,
+  setLightboxOpen: jest.fn(),
+  photoIndex: 1,
+  setPhotoIndex: jest.fn(),
+};
+
+describe('<FilterButtons/>', () => {
+  test('selected button', async () => {
+    const { getByRole } = render(<FilterButtons {...props} />);
+
+    const selectedBtn = getByRole('button', { name: /^selected/i });
+    const allBtn = getByRole('button', { name: /^all/i });
+
+    user.click(selectedBtn);
+
+    expect(props.setFilteredPhotos).toHaveBeenLastCalledWith([
+      collection.photos[1],
+    ]);
+
+    user.click(allBtn);
+
+    expect(props.setFilteredPhotos).toHaveBeenLastCalledWith(collection.photos);
+  });
+
+  test('not selected button', () => {
+    const { getByRole } = render(<FilterButtons {...props} />);
+
+    const notSelectedBtn = getByRole('button', { name: /^not selected/i });
+    const allBtn = getByRole('button', { name: /^all/i });
+
+    user.click(notSelectedBtn);
+
+    expect(props.setFilteredPhotos).toHaveBeenLastCalledWith([
+      collection.photos[0],
+    ]);
+
+    user.click(allBtn);
+
+    expect(props.setFilteredPhotos).toHaveBeenLastCalledWith(collection.photos);
+  });
+});
+
+describe('<FilterButtons modifyLightbox/>', () => {
+  test('photoIndex is higher or equal to filtered photos length', () => {
+    const { getByRole } = render(<FilterButtons {...propsModifyLightbox} />);
+
+    const selectedBtn = getByRole('button', { name: /^selected/i });
+
+    user.click(selectedBtn);
+
+    expect(propsModifyLightbox.setPhotoIndex).toHaveBeenCalledWith(0);
+  });
+  test('filtered photos become zero', () => {
+    propsModifyLightbox.collection.photos[1].selected = false;
+    const { getByRole } = render(<FilterButtons {...propsModifyLightbox} />);
+
+    const selectedBtn = getByRole('button', { name: /^selected/i });
+
+    user.click(selectedBtn);
+
+    expect(propsModifyLightbox.setLightboxOpen).toHaveBeenCalledWith(false);
+  });
+});
